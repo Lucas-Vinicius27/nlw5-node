218ca8f4bf140dad294a58b5f80d65ca3db4646d
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-04-23 00:32:31 -0300

    Update 1619133355801-CreateMessages.ts

--- src/database/migrations/1619133355801-CreateMessages.ts
+++ src/database/migrations/1619133355801-CreateMessages.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateMessages1619133355801 implements MigrationInterface {
 
