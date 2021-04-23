0239d3171d0c9388b5adaeed4ec4bfeb7587384a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-04-23 00:31:39 -0300

    Update 1619060874761-CreateSettings.ts

--- src/database/migrations/1619060874761-CreateSettings.ts
+++ src/database/migrations/1619060874761-CreateSettings.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateSettings1619060874761 implements MigrationInterface {
 
