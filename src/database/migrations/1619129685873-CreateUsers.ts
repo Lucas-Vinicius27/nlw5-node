9489d5722150d4ef5770b28a960d47d06094c54a
Lucas Vinicius <lucas_vfsantos@hotmail.com>
2021-04-23 00:32:04 -0300

    Update 1619129685873-CreateUsers.ts

--- src/database/migrations/1619129685873-CreateUsers.ts
+++ src/database/migrations/1619129685873-CreateUsers.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateUsers1619129685873 implements MigrationInterface {
 
